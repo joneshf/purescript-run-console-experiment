e7b7c1727a93994a4ba7f03c538d53156c522353
joneshf <jones3.hardy@gmail.com>
2017-09-16 21:39:23 -0700

    Document some functions

--- src/Run/Console.purs
+++ src/Run/Console.purs
@@ -14,6 +14,7 @@ import Data.Symbol (SProxy(..))
 import Data.Unit (Unit, unit)
 import Run (BaseEff, Run, interpret, liftEffect, peel, send)
 
+-- | The possible messages we can have on the console
 data Console a
   = Error String a
   | Info String a
@@ -25,6 +26,7 @@ derive instance functorConsole :: Functor Console
 type CONSOLE
   = FProxy Console
 
+-- | A helper for the `console` label used in row types.
 _console :: SProxy "console"
 _console = SProxy
 
@@ -52,35 +54,10 @@ warn str = liftEffect _console (Warn str unit)
 warnShow :: forall a r. Show a => a -> Run (console :: CONSOLE | r) Unit
 warnShow x = liftEffect _console (Warn (show x) unit)
 
-runConsole
-  :: forall a e r
-  . Run (console :: CONSOLE | r) a
-  -> Run (base :: BaseEff (console :: Eff.CONSOLE | e) | r) a
-runConsole = interpret _console case _ of
-  Error str x -> x <$ Eff.error str
-  Info str x -> x <$ Eff.info str
-  Log str x -> x <$ Eff.log str
-  Warn str x -> x <$ Eff.warn str
-
-runNoConsole
-  :: forall a r
-  . Run (console :: CONSOLE | r) a
-  -> Run r a
-runNoConsole = runPure case _ of
-  Error _ w -> w
-  Info _ w -> w
-  Log _ w -> w
-  Warn _ w -> w
-
-runPure
-  :: forall a r
-  . (Console (Run (console :: CONSOLE | r) a) -> Run (console :: CONSOLE | r) a)
-  -> Run (console :: CONSOLE | r) a
-  -> Run r a
-runPure f = go
-  where
-  go x = either (on _console (go <<< f) (bindFlipped go <<< send)) pure (peel x)
-
+-- | Accumulates all console messages into a list
+-- | but does not print any to the console.
+-- |
+-- | Useful when you want to see what would be printed to the console.
 runAccumulate
   :: forall a r
   . Run (console :: CONSOLE | r) a
@@ -92,3 +69,42 @@ runAccumulate x' = reverse <$> runPure cons (Nil <$ x')
     Info str w -> Cons str <$> w
     Log str w -> Cons str <$> w
     Warn str w -> Cons str <$> w
+
+-- | Prints all messages to the console.
+-- |
+-- | Normally this is what you'll use to print to the console.
+runConsole
+  :: forall a e r
+  . Run (console :: CONSOLE | r) a
+  -> Run (base :: BaseEff (console :: Eff.CONSOLE | e) | r) a
+runConsole = interpret _console case _ of
+  Error str x -> x <$ Eff.error str
+  Info str x -> x <$ Eff.info str
+  Log str x -> x <$ Eff.log str
+  Warn str x -> x <$ Eff.warn str
+
+-- | Runs without printing any messages to the console.
+-- |
+-- | Useful when you want to eliminate the `CONSOLE` effect
+-- | without printing anything to the console.
+runNoConsole
+  :: forall a r
+  . Run (console :: CONSOLE | r) a
+  -> Run r a
+runNoConsole = runPure case _ of
+  Error _ w -> w
+  Info _ w -> w
+  Log _ w -> w
+  Warn _ w -> w
+
+-- | Runs the given function in a pure context eliminating the `CONSOLE` effect.
+-- |
+-- | Useful for building up new pure combinators.
+runPure
+  :: forall a r
+  . (Console (Run (console :: CONSOLE | r) a) -> Run (console :: CONSOLE | r) a)
+  -> Run (console :: CONSOLE | r) a
+  -> Run r a
+runPure f = go
+  where
+  go x = either (on _console (go <<< f) (bindFlipped go <<< send)) pure (peel x)
